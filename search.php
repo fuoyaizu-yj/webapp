49666d1518f33c9f70e223895513430ca605f91b
fuyuki oyaizu <fuoyaizu@yahoo-corp.jp>
2017-05-15 13:57:20 +0900

    Create search.php

---
+++ search.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<meta charset="UTF-8">
+<title>特集検索サンプルページ</title> 
+</head> 
+<body>
+
+<h1>特集検索サンプルページ</h1>
+<form>
+<fieldset>
+キーワード：<input type="text" name="searchword" autofocus required><br>
+<input type="submit" value="送信">
+</fieldset>
+</form>
+
+</body> 
+</html>
+
